3816f5239f9cc98d7869e75d027c1e5438831d0b
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-16 17:53:22 +0100

    🎯 F02 : Amélioration de la recherche dans Banner avec navigation dynamique

--- src/components/pages/Banner.jsx
+++ src/components/pages/Banner.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../style/Banner.css'
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import { fakeMenu } from '../../fakeData/fakeMenu';
 
 const Banner = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const lowerSearch = searchValue.trim().toLowerCase();
+
+        if (!lowerSearch) return;
+
+        let foundRecipe = null;
+
+        // Cherche dans chaque catégorie
+        fakeMenu.Category.forEach(category => {
+            const match = category.recettes?.find(recette =>
+                recette.title.toLowerCase().includes(lowerSearch)
+            );
+            if (match) foundRecipe = { ...match, categoryTitle: category.title };
+        });
+
+        if (foundRecipe) {
+            navigate(`/category/${foundRecipe.categoryTitle}/${foundRecipe.id}`);
+
+        } else {
+            alert("Recette introuvable !");
+        }
+    };
     return (
         <div className='banner'>
             <div className='banner-content'>
-                <form className='formulaire'>
+                <form className='formulaire' onSubmit={handleSubmit}>
                     <div className='formulaire-content'>
-                        <input type='search' id='search_input' placeholder='Je cherche une recette, un ingrédient...' className='input-search'/>
+                        <input 
+                            type='search' 
+                            id='search_input' 
+                            placeholder='Je cherche une recette, un ingrédient...' 
+                            className='input-search' 
+                            value={searchValue} 
+                            onChange={(e) => setSearchValue(e.target.value)}
+                        />
                         <div className='icon-wrapper'>
                             <FaSearch className='search-icon' />
                         </div>
