6725f4c84868a0b18bfadfde573eec98b65e4cfc
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-16 19:44:18 +0100

    ✨ F03 : Ajout des composants Category, TopFood et SpecialRecipes à <Home.jsx/>

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,17 +3,21 @@ import Banner from './Banner';
 import '../../style/Home.css'
 import Category from './Category';
 import CurrentNews from './CurrentNews';
-//import TopFood from './TopFood';
+import TopFood from './TopFood';
 import Recipes from './Recipes';
+//import News from './News';
+import SpecialRecipes from './SpecialRecipes';
 
 const Home = () => {
     return (
         <div className='home'>
             <Banner/>
-            {/*<Category/>*/}
+            <Category/>
             <CurrentNews />
-            {/*<TopFood />*/}
-            {/*<Recipes/>*/}
+            <TopFood />
+            {/*<News />*/}
+            <Recipes/>
+            <SpecialRecipes />
         </div>
     );
 }
