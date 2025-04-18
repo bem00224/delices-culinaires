a6d5923279e172466c959c562773ecdb40cdf934
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-18 12:13:21 +0100

    💄F04 : Ajout du style pour le composant <Menu/>

--- src/components/pages/sections/Menu.jsx
+++ src/components/pages/sections/Menu.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import Title from '../../reusable-ui/Title';
 import Cards from '../../reusable-ui/Cards';
+import "../../../style/Menu.css"
 
 const Menu = ({title, data, basePath= ''}) => {
     return (
         <div className='section-cards'>
             <Title title={title} />
             <div className='section-cards-container'>
-                {
-                    data.map(({id, title, imageSource}) => (
-                        <Cards
-                            key={id}
-                            to={`${basePath}/${title}`}
-                            image={imageSource}
-                            title={title}
-                        />
-                    ) )
-                }
+                <div className='section-cards-detail'>
+                    {
+                        data.map(({id, title, imageSource}) => (
+                            <Cards
+                                key={id}
+                                to={`${basePath}/${title}`}
+                                image={imageSource}
+                                title={title}
+                            />
+                        ) )
+                    }
+                </div>
             </div>
         </div>
     );
