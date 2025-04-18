7fa818ecba27751b0733261450a29f7e7979aa7e
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-18 10:32:19 +0100

    ✨ F03 : Ajout du composant génerique <Title /> dans ( <TopFood/>, <CurrentNews/> et <Recipes />)

--- src/components/pages/Recipes.jsx
+++ src/components/pages/Recipes.jsx
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import { fakeMenu } from '../../fakeData/fakeMenu';
 import Cards from '../reusable-ui/Cards';
 import '../../style/Recipes.css'
+import Title from '../reusable-ui/Title';
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState(fakeMenu.Recipes)
     return (
         <div className='recipes'>
-            <div className='recipes-description'>
-                <h3 className='recipes-title'>Recettes</h3>
-            </div>
+            <Title title="Recettes" />
             <div className='recipes-card'>
                 <div className='recipes-card-content'>
                     <div className='recipes-card-detail' >
                         {
                             recipes.map((recipe) => (
                                 <Cards 
-                                    to={"/recipes"}
+                                    to={`/recipes/${recipe.title}`}
                                     key={recipe.id}
                                     image={recipe.imageSource}
                                     title={recipe.title}
