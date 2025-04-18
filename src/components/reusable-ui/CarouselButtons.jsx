780cbc21f04188ee03445ed628273cedf1356d47
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-18 23:19:36 +0100

    ✨ F04 :  Creation de <CarouselButtons/>

---
+++ src/components/reusable-ui/CarouselButtons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarouselButtons = () => {
+    return (
+        <div>
+            CarouselButtons
+        </div>
+    );
+}
+
+export default CarouselButtons;
