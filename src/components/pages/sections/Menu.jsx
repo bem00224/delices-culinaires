4cb4f6aa8b05ee235179fdad90b60402e626f37b
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-18 11:18:35 +0100

    ✨ F04 : Ajout du titre, des données à afficher et la route de redirection de base

--- src/components/pages/sections/Menu.jsx
+++ src/components/pages/sections/Menu.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Title from '../../reusable-ui/Title';
+import Cards from '../../reusable-ui/Cards';
 
-const Menu = () => {
+const Menu = ({title, data, basePath= ''}) => {
     return (
-        <div>
-            Menu
+        <div className='section-cards'>
+            <Title title={title} />
+            <div className='section-cards-container'>
+                {
+                    data.map(({id, title, imageSource}) => (
+                        <Cards
+                            key={id}
+                            to={`${basePath}/${title}`}
+                            image={imageSource}
+                            title={title}
+                        />
+                    ) )
+                }
+            </div>
         </div>
     );
 }
